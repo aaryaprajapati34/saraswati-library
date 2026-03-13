06a9803e908a793e962888a00b4ec90ba0d46463
aaryaprajapati <aaryaprajapati34@gmail.com>
2026-03-14 01:25:28 +0530

    fix production API url

--- static/js/config.js
+++ static/js/config.js
@@ -3,9 +3,12 @@ const API_BASE_URL = {
     production: "https://saraswati-library.onrender.com"
 };
 
-const isProduction = window.location.hostname !== "127.0.0.1" && 
-                     window.location.hostname !== "localhost";
+const isProduction =
+    window.location.hostname !== "127.0.0.1" &&
+    window.location.hostname !== "localhost";
 
-const API_URL = isProduction ? API_BASE_URL.production : API_BASE_URL.development;
+const API_URL = isProduction
+    ? API_BASE_URL.production
+    : API_BASE_URL.development;
 
 window.API_URL = API_URL;
